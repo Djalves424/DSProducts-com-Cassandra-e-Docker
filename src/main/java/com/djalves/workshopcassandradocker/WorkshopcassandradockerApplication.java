ba08168eb37137fdf7c9815a25f3a5d42b7d9f64
Diego Alves <djalves424@gmail.com>
2024-04-05 17:25:15 -0300

    Project created Cassandra with Docker

---
+++ src/main/java/com/djalves/workshopcassandradocker/WorkshopcassandradockerApplication.java
@@ -0,0 +1,13 @@
+package com.djalves.workshopcassandradocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopcassandradockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopcassandradockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djalves/workshopcassandradocker/WorkshopcassandradockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djalves.workshopcassandradocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopcassandradockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
