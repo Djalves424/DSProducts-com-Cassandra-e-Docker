be9735bc53d862ba3b3aed4309672bdf8ca9f60b
Diego Alves <djalves424@gmail.com>
2024-04-05 18:52:36 -0300

    Created the Department class in the entities layer

---
+++ src/main/java/com/djalves/workshopcassandradocker/model/entities/Department.java
@@ -0,0 +1,39 @@
+package com.djalves.workshopcassandradocker.model.entities;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.UUID;
+
+@Table(value = "departments")
+public class Department {
+
+    @PrimaryKey
+    private UUID id;
+    private String name;
+
+    public Department() {
+
+    }
+
+    public Department(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
